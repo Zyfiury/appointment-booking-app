ca53af5f91c59b717377187ca7c7476c8d1ca2d5
Omar <omar.zakyy2005@gmail.com>
2026-01-16 12:01:24 +0000

    Explicitly set Google Sign-In serverClientId

--- lib/services/google_auth_service.dart
+++ lib/services/google_auth_service.dart
@@ -3,13 +3,11 @@ import 'package:dio/dio.dart';
 import 'api_service.dart';
 
 class GoogleAuthService {
-  // Use the serverClientId from AndroidManifest.xml (default_web_client_id)
-  // This is automatically read from strings.xml via AndroidManifest meta-data
+  // Explicitly set serverClientId to ensure it's used
+  // This is the OAuth client ID from Google Cloud Console
   final GoogleSignIn _googleSignIn = GoogleSignIn(
     scopes: ['email', 'profile'],
-    // serverClientId is automatically read from AndroidManifest.xml meta-data
-    // If that doesn't work, uncomment and set explicitly:
-    // serverClientId: '621611382404-3su41a8p8bucc44leffirbva6a3eac8p.apps.googleusercontent.com',
+    serverClientId: '621611382404-3su41a8p8bucc44leffirbva6a3eac8p.apps.googleusercontent.com',
   );
   final ApiService _apiService = ApiService();
 
